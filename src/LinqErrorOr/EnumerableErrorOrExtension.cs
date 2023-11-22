aaac05f5d2b353f2ab1282c5195854a1aff3dd78
mzdyhrave <info@hravemzdy.org>
2023-11-22 17:54:46 +0100

    ServiceProcezlyCz

---
+++ src/LinqErrorOr/EnumerableErrorOrExtension.cs
@@ -0,0 +1,52 @@
+﻿namespace ErrorOr;
+
+public static class EnumerableErrorOrExtension
+{
+
+    // Functor aggregate
+    public static ErrorOr<TAccumulate> AggregateErrorOr<TSource, TAccumulate>(this IEnumerable<TSource> source, 
+        TAccumulate seed, Func<ErrorOr<TAccumulate>, TSource, ErrorOr<TAccumulate>> func)
+    {
+        return source.Aggregate(ErrorOrResult<TAccumulate>.Success(seed), func);
+    }
+    // Functor map
+    public static ErrorOr<IEnumerable<TA>> SelectErrorOr<TA>(this IEnumerable<ErrorOr<TA>> ma)
+    {
+        var errors = ma.Where(a => a.IsError).SelectMany(b => b.Errors).ToList();
+        if (errors.Count != 0)
+        {
+            return ErrorOr<IEnumerable<TA>>.From(errors);
+        }
+        return ma.Where(a => a.IsError==false).Select(b => b.Value).ToList();
+    }
+    public static ErrorOr<IEnumerable<TB>> SelectErrorOr<TA, TB>(this IEnumerable<ErrorOr<TA>> ma, Func<TA, TB> f)
+    {
+        var errors = ma.Where(a => a.IsError).SelectMany(b => b.Errors).ToList();
+        if (errors.Count != 0)
+        {
+            return ErrorOr<IEnumerable<TB>>.From(errors);
+        }
+        return ma.Where(a => a.IsError==false).Select(x => f(x.Value)).ToList();
+    }
+    // Functor map
+    public static ErrorOr<IDictionary<TK, IEnumerable<TA>>> SelectErrorOr<TK, TA>(this IDictionary<TK, ErrorOr<IEnumerable<TA>>> ma)
+        where TK : notnull
+    {
+        var errors = ma.Where(a => a.Value.IsError).SelectMany(b => b.Value.Errors).ToList();
+        if (errors.Count != 0)
+        {
+            return ErrorOr<IDictionary<TK, IEnumerable<TA>>>.From(errors);
+        }
+        return ma.Where(a => a.Value.IsError==false).ToDictionary(k => k.Key, v => v.Value.Value);
+    }
+    public static ErrorOr<IDictionary<TK, IEnumerable<TB>>> SelectErrorOr<TK, TA, TB>(this IDictionary<TK, ErrorOr<IEnumerable<TA>>> ma, Func<TA, TB> f)
+        where TK : notnull
+    {
+        var errors = ma.Where(a => a.Value.IsError).SelectMany(b => b.Value.Errors).ToList();
+        if (errors.Count != 0)
+        {
+            return ErrorOr<IDictionary<TK, IEnumerable<TB>>>.From(errors);
+        }
+        return ma.Where(a => a.Value.IsError==false).ToDictionary(k => k.Key, v => v.Value.Value.Select(x => f(x)));
+    }
+}
