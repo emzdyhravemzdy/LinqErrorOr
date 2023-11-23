cfe80887be2abbcf562a5c30071b6487104fb3b0
mzdyhrave <info@hravemzdy.org>
2023-11-23 10:33:18 +0100

    Release 0.0.5.

--- src/LinqErrorOr/EnumerableErrorOrExtension.cs
+++ src/LinqErrorOr/EnumerableErrorOrExtension.cs
@@ -2,14 +2,48 @@
 
 public static class EnumerableErrorOrExtension
 {
-    private static IReadOnlyList<ErrorOr<TA>> UnWrap<TA>(this ErrorOr<IReadOnlyList<ErrorOr<TA>>> ma)
+    // Functor UnWrap
+    public static IReadOnlyList<ErrorOr<TA>> UnWrap<TA>(this ErrorOr<IReadOnlyList<ErrorOr<TA>>> ma)
     {
         return ma.Match(
            onError: err => MakeErrorOr<TA>.MapErrors(err),
            onValue: res => res);
     }
+    public static IEnumerable<TA> SuccessList<TA>(this IEnumerable<ErrorOr<TA>> ma) where TA : class
+    {
+        foreach (ErrorOr<TA> item in ma)
+        {
+            if (item.IsError == false)
+            {
+                yield return item.Value;
+            }
+        }
+    }
+    public static IEnumerable<Error> FailuresList<TA>(this IEnumerable<ErrorOr<TA>> ma) where TA : class
+    {
+        foreach (ErrorOr<TA> item in ma)
+        {
+            if (item.IsError)
+            {
+                yield return item.FirstError;
+            }
+        }
+    }
+    public static IEnumerable<Error> ErrorsList<TA>(this IEnumerable<ErrorOr<TA>> ma) where TA : class
+    {
+        foreach (ErrorOr<TA> item in ma)
+        {
+            if (item.IsError)
+            {
+                foreach (Error err in item.Errors)
+                {
+                    yield return err;
+                }
+            }
+        }
+    }
     // Functor aggregate
-    public static ErrorOr<TAccumulate> AggregateErrorOr<TSource, TAccumulate>(this IEnumerable<TSource> source, 
+    public static ErrorOr<TAccumulate> AggregateErrorOr<TSource, TAccumulate>(this IEnumerable<TSource> source,
         TAccumulate seed, Func<ErrorOr<TAccumulate>, TSource, ErrorOr<TAccumulate>> func)
     {
         return source.Aggregate(MakeErrorOr<TAccumulate>.Success(seed), func);
@@ -22,7 +56,7 @@ public static class EnumerableErrorOrExtension
         {
             return ErrorOr<IEnumerable<TA>>.From(errors);
         }
-        return ma.Where(a => a.IsError==false).Select(b => b.Value).ToList();
+        return ma.Where(a => a.IsError == false).Select(b => b.Value).ToList();
     }
     public static ErrorOr<IEnumerable<TB>> SelectErrorOr<TA, TB>(this IEnumerable<ErrorOr<TA>> ma, Func<TA, TB> f)
     {
@@ -31,7 +65,7 @@ public static class EnumerableErrorOrExtension
         {
             return ErrorOr<IEnumerable<TB>>.From(errors);
         }
-        return ma.Where(a => a.IsError==false).Select(x => f(x.Value)).ToList();
+        return ma.Where(a => a.IsError == false).Select(x => f(x.Value)).ToList();
     }
     // Functor map
     public static ErrorOr<IDictionary<TK, IEnumerable<TA>>> SelectErrorOr<TK, TA>(this IDictionary<TK, ErrorOr<IEnumerable<TA>>> ma)
@@ -42,7 +76,7 @@ public static class EnumerableErrorOrExtension
         {
             return ErrorOr<IDictionary<TK, IEnumerable<TA>>>.From(errors);
         }
-        return ma.Where(a => a.Value.IsError==false).ToDictionary(k => k.Key, v => v.Value.Value);
+        return ma.Where(a => a.Value.IsError == false).ToDictionary(k => k.Key, v => v.Value.Value);
     }
     public static ErrorOr<IDictionary<TK, IEnumerable<TB>>> SelectErrorOr<TK, TA, TB>(this IDictionary<TK, ErrorOr<IEnumerable<TA>>> ma, Func<TA, TB> f)
         where TK : notnull
@@ -52,6 +86,6 @@ public static class EnumerableErrorOrExtension
         {
             return ErrorOr<IDictionary<TK, IEnumerable<TB>>>.From(errors);
         }
-        return ma.Where(a => a.Value.IsError==false).ToDictionary(k => k.Key, v => v.Value.Value.Select(x => f(x)));
+        return ma.Where(a => a.Value.IsError == false).ToDictionary(k => k.Key, v => v.Value.Value.Select(x => f(x)));
     }
 }
